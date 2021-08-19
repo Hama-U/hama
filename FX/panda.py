357c7d64601ba346934defeef82172a110ebaf46
hama you <87755388+Hama-U@users.noreply.github.com>
2021-08-19 13:04:24 +0900

    Delete FX directory

--- FX/panda.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd     #pandaの導入, コマンドラインで%pip3 install pandasと打つ
-
-dataset_test = pd.read_csv("test.csv")   #detasetという変数にtestdataを読み込む
-#dataset_fx = pd.read_csv("testdata.csv")  #多分日本語が入ってるからエラーはく
-
-print(dataset_test)      #読み込めたか出力してみる
-#print(dataset_fx)
-
